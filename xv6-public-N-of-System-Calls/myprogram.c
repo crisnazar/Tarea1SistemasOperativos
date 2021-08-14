233f839cb5caea13221e73f229d550c063ae90c3
crisnazar <62630999+crisnazar@users.noreply.github.com>
2021-08-14 15:30:29 -0400

    Add files via upload

---
+++ xv6-public-N-of-System-Calls/myprogram.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(void)
+{
+printf( 1,"Total Number of System Calls till now are: %d\n" , test() );
+exit();
+}
